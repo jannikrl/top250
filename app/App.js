3837baa238e83f0354270840f31ac9d59bbccf35
jannik <jannik.rl@gmail.com>
2018-04-04 12:47:16 +0200

    project setup

---
+++ app/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import {
+  Platform,
+  Text,
+  View
+} from 'react-native';
+
+export default class App extends Component {
+  render() {
+    return (
+      <View>
+        <Text>
+          Hello World!
+        </Text>
+      </View>
+    );
+  }
+}
---
+++ app/assets/styles/varibales.js
@@ -0,0 +1,18 @@
+import Color from 'color';
+
+export const
+    // Color
+    primaryColor = Color('#2C89F9'),
+    textColor = Color('#ffffff'),
+    backgroundColor = Color('#252525'),
+
+    // Form
+    basePadding = 8,
+    baseMargin = 8,
+    borderRadius = 4,
+
+    // Shadow
+    shadowColor = Color('#000000'),
+    shadowOffset = {width: 0, height: 2},
+    shadowRadius = 4,
+    shadowOpacity = 0.5;
